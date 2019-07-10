7b909a2b781effdb20fe1de360033160472d5dfb
Cabdulahi Sharif Mohammed <33965166+Cabdulahi@users.noreply.github.com>
2019-07-10 16:57:54 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,1 @@
+pkg install python;pkg install python2;pip2 install --upgrade pip;pip2 install --upgrade pip;pip2 install --upgrade pip;pip2 install requests;pip2 install bs4;pip2 install BeautifulSoup;pip2 install mechanize;pip2 install 
